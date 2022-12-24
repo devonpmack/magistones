2f465fe2f1cb1ee13faab86fb3eda5b580f7b163
Devon Mack <devonpmack@gmail.com>
2022-12-23 21:16:38 -0500

    Color the damage text

--- Assets/DamageDisplay.cs
+++ Assets/DamageDisplay.cs
@@ -7,5 +7,8 @@ public class DamageDisplay : MonoBehaviour {
   void Update() {
     GetComponent<TMPro.TextMeshPro>().text = wizard.Damage.ToString() + '%';
     transform.LookAt(Camera.main.transform.position);
+
+    // slowly change the text color to red as it approaches 250
+    GetComponent<TMPro.TextMeshPro>().color = Color.Lerp(Color.white, Color.red, wizard.Damage / 170f);
   }
 }
